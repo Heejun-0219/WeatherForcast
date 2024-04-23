22aed2f17dd1a7d0e980c98f3157c0637e8ab381
HeeJune KIM <heejun0219@gmail.com>
2024-04-23 14:36:59 +0900

    초기 커밋.

---
+++ WeatherForcast/WeatherForcast.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <mysql.h>
+
+#pragma comment(lib, "libmysql.lib")
+
+int main()
+{
+    MYSQL* conn;
+
+    conn = mysql_init(0);
+    std::cout << "Hello World!\n";
+}
