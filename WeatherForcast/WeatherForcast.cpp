5afa556d8c86d3aeccffdae30bea815276a37995
HeeJune KIM <heejun0219@gmail.com>
2024-04-24 14:03:17 +0900

    github action test

--- WeatherForcast/WeatherForcast.cpp
+++ WeatherForcast/WeatherForcast.cpp
@@ -1,9 +1,12 @@
 ﻿#include <iostream>
-//#include <mysql.h>
-//
-//#pragma comment(lib, "libmysql.lib")
+#include <mysql.h>
+
+#pragma comment(lib, "libmysql.lib")
 
 int main()
 {
+    MYSQL* conn;
+
+    conn = mysql_init(0);
     std::cout << "Hello World!\n";
 }
